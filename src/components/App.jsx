8d8a043850700663476413ce9c9bb46638f5c7cd
Bryhilevych Orest <91126054+OrestBruhylevych@users.noreply.github.com>
2022-07-17 17:15:14 +0300

    sar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,23 @@
-export const App = () => {         
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
+
+
+
+
+
+
+
+
+
+
+
+
+        
         alignItems: 'center',
         fontSize: 40,
         color: '#010101',
